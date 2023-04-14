5642e67a9bc50a71800a957cd65d02d626f97d8b
Santiago Garc=ia <santiago.garcia@posteo.de>
2023-04-14 08:41:56 +0200

    Rainbow brackets

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,6 +1,13 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  dependencies = {
+    "mrjones2014/nvim-ts-rainbow",
+  },
   opts = {
     -- ensure_installed = { "lua" },
+    rainbow = {
+      enable = true,
+      extended_mode = true
+    }
   },
 }
