f9d9f2f4cd8c9ba077b5100832761c0beec17540
Santiago Garc=ia <santiago.garcia@posteo.de>
2023-04-23 22:55:35 +0200

    Signature help

---
+++ plugins/cmp.lua
@@ -0,0 +1,7 @@
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = { "hrsh7th/cmp-nvim-lsp-signature-help" },
+    opts = function(_, config) table.insert(config.sources, { name = "nvim_lsp_signature_help" }) end,
+  },
+}
