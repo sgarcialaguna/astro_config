0b04f09163d44aa927c92c168cdc2b97f4cb5b32
Santiago Garcia <santiago.garcia@posteo.de>
2023-05-04 08:50:46 +0200

    Try fork of nvim-cmp

--- plugins/cmp.lua
+++ plugins/cmp.lua
@@ -1,21 +1,21 @@
 return {
   {
-    "hrsh7th/nvim-cmp",
+    "yioneko/nvim-cmp",
     dependencies = { "ray-x/lsp_signature.nvim" },
     opts = function(_, config)
       table.insert(config.sources, { name = "nvim_lsp_signature_help" })
-
+  
       local cmp = require "cmp"
       local luasnip = require "luasnip"
       function name()
         -- code
       end
-
+  
       local function has_words_before()
         local line, col = unpack(vim.api.nvim_win_get_cursor(0))
         return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
       end
-
+  
       config.mapping["<Tab>"] = cmp.mapping(function(fallback)
         if luasnip.expand_or_jumpable() then
           luasnip.expand_or_jump()
@@ -27,7 +27,7 @@ return {
           fallback()
         end
       end, { "i", "s" })
-
+  
       config.mapping["<S-Tab>"] = cmp.mapping(function(fallback)
         if luasnip.jumpable(-1) then
           luasnip.jump(-1)
@@ -37,13 +37,13 @@ return {
           fallback()
         end
       end, { "i", "s" })
-
-      config.performance = {
-        debounce = 300,
-        throttle = 60,
-        fetching_timeout = 200,
-      }
-
+  
+      -- config.performance = {
+      --   debounce = 300,
+      --   throttle = 100,
+      --   fetching_timeout = 50,
+      -- }
+  
       return config
     end,
   },
