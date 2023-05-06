3dac27e85d9ee048636688695a23cb8fd67556fa
Santiago Garcia <santiago.garcia@posteo.de>
2023-05-06 09:36:23 +0200

    Tokyonight colorscheme

---
+++ plugins/tokyonight.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    config = {
+      styles = {
+        comments = { italic = false },
+        keywords = { italic = false },
+      },
+    },
+  },
+}
